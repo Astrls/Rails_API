ae8f2b8a6c2eaee13b8b0290cc8e1a650a831a4e
Antoine Struelens <antoine.struelens@gmail.com>
2023-09-25 16:39:29 +0200

    snapshot before resetting devise model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :v1 do
     resources :profiles
+    resources :sessions, only: [:create, :destroy]
   end
 
   
